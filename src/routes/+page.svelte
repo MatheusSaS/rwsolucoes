b67a00b3b68abab4e2321e503818ed84febd535d
Matheus Sá <matheus.souza410@etec.sp.gov.br>
2023-06-21 20:48:31 -0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,339 @@
+<!-- hero - start -->
+<div class="bg-white pb-6 sm:pb-8 lg:pb-12">
+  <header class="border-b bg-black">
+    <div
+      class="mx-auto flex max-w-screen-2xl items-center justify-center px-4 md:px-8"
+    >
+      <!-- logo - start -->
+      <a
+        href="/"
+        class="text-black-800 inline-flex items-center gap-2.5 text-2xl font-bold md:text-3xl p-2"
+        aria-label="logo"
+      >
+        <img src="/rw.png" alt="logo" class="h-auto w-32" />
+      </a>
+    </div>
+  </header>
+
+  <div class="mx-auto w-full -mt-1">
+    <section
+      class="min-h-96 relative flex flex-1 shrink-0 items-center justify-center overflow-hidden bg-gray-100 py-16 shadow-lg md:py-20 xl:py-48"
+    >
+      <!-- image - start -->
+      <img
+        src="/cano2.jpg?auto=format&amp;q=75&amp;fit=crop&amp;w=1500"
+        loading="lazy"
+        alt="Photo by Fakurian Design"
+        class="absolute inset-0 h-full w-full object-cover object-center"
+        style="opacity: 1;"
+      />
+      <!-- image - end -->
+
+      <!-- overlay - start -->
+      <div class="absolute inset-0 bg-black/50 mix-blend-multiply" />
+      <!-- overlay - end -->
+
+      <!-- text start -->
+      <div class="relative flex flex-col items-center p-4 sm:max-w-3xl">
+        <p class="mb-4 text-center text-lg text-emerald-200 sm:text-xl md:mb-8">
+          Assessoria em processos de soldagem
+        </p>
+        <h1
+          class="mb-8 text-center text-4xl font-bold text-white sm:text-4xl md:mb-12 md:text-6xl"
+        >
+          RW Soluções industriais
+        </h1>
+
+        <div class="flex w-full flex-col gap-2.5 sm:flex-row sm:justify-center">
+          <a
+            href="https://wa.me/5517996453211"
+            class="btn bg-emerald-500 hover:bg-emerald-600 text-base-100"
+          >
+            <i class="fa-brands fa-whatsapp mr-1 fa-xl" />Entrar em contato</a
+          >
+        </div>
+      </div>
+      <!-- text end -->
+    </section>
+  </div>
+</div>
+<!-- hero - end -->
+
+<!-- product-grid - start -->
+<div class="bg-white py-6 sm:py-8 lg:py-12">
+  <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+    <div class="mb-4 flex items-center justify-between gap-8 sm:mb-8 md:mb-12">
+      <div class="flex items-center gap-12">
+        <h2 class="text-2xl font-bold text-gray-800 lg:text-3xl">Trabalhos</h2>
+
+        <p class="hidden max-w-screen-sm text-gray-500 md:block">
+          Fique por dentro do nosso trabalho e saiba como podemos ajudá-lo
+        </p>
+      </div>
+    </div>
+
+    <div class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8">
+      <!-- image - start -->
+      <a
+        href="#"
+        class="group relative flex h-48 items-end overflow-hidden bg-gray-100 shadow-lg md:h-80"
+      >
+        <img
+          src="/img-1.png?auto=format&q=75&fit=crop&w=600"
+          loading="lazy"
+          alt="Photo by Minh Pham"
+          class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+        />
+
+        <div
+          class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"
+        />
+
+        <span
+          class="relative ml-4 mb-3 inline-block text-xs sm:text-sm text-white md:ml-5 md:text-lg"
+          >Fabricação de escadas aço e Madeira</span
+        >
+      </a>
+      <!-- image - end -->
+
+      <!-- image - start -->
+      <a
+        href="#"
+        class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80"
+      >
+        <img
+          src="/img-2.png?auto=format&q=75&fit=crop&w=1000"
+          loading="lazy"
+          alt="Photo by Magicle"
+          class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+        />
+
+        <div
+          class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"
+        />
+
+        <span
+          class="relative ml-4 mb-3 inline-block text-xs sm:text-sm text-white md:ml-5 md:text-lg"
+          >Fabricação de guarda corpos e Pergolados</span
+        >
+      </a>
+      <!-- image - end -->
+
+      <!-- image - start -->
+      <a
+        href="#"
+        class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80"
+      >
+        <img
+          src="/img-3.png?auto=format&q=75&fit=crop&w=1000"
+          loading="lazy"
+          alt="Photo by Martin Sanchez"
+          class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+        />
+
+        <div
+          class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"
+        />
+
+        <span
+          class="relative ml-4 mb-3 inline-block text-xs sm:text-sm text-white md:ml-5 md:text-lg"
+          >Estrutura para placa solar</span
+        >
+      </a>
+      <!-- image - end -->
+
+      <!-- image - start -->
+      <a
+        href="#"
+        class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
+      >
+        <img
+          src="/cano.jpg?auto=format&q=75&fit=crop&w=600"
+          loading="lazy"
+          alt="Photo by Lorenzo Herrera"
+          class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+        />
+
+        <div
+          class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"
+        />
+
+        <span
+          class="relative ml-4 mb-3 inline-block text-xs sm:text-sm text-white md:ml-5 md:text-lg"
+          >Acabamento em solda tig</span
+        >
+      </a>
+      <!-- image - end -->
+    </div>
+  </div>
+</div>
+<!-- product-grid - end -->
+
+<!-- call to action - start -->
+<div class="bg-white py-6 sm:py-8 lg:py-12">
+  <div class="mx-auto max-w-screen-lg px-4 md:px-8">
+    <!-- text - start -->
+    <div class="mb-8 md:mb-12">
+      <h2
+        class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl"
+      >
+        Nossa equipe pelos números
+      </h2>
+    </div>
+    <!-- text - end -->
+
+    <div
+      class="grid grid-cols-2 gap-6 rounded-lg bg-emerald-500 p-6 md:grid-cols-4 md:gap-8 md:p-8"
+    >
+      <!-- stat - start -->
+      <div class="flex flex-col items-center">
+        <div class="text-xl font-bold text-white sm:text-2xl md:text-3xl">
+          200
+        </div>
+        <div class="text-sm text-emerald-200 sm:text-base">
+          Projetos concluídos
+        </div>
+      </div>
+      <!-- stat - end -->
+
+      <!-- stat - start -->
+      <div class="flex flex-col items-center">
+        <div class="text-xl font-bold text-white sm:text-2xl md:text-3xl">
+          500+
+        </div>
+        <div class="text-sm text-emerald-200 sm:text-base">
+          Nossos equipamentos
+        </div>
+      </div>
+
+      <!-- stat - start -->
+      <div class="flex flex-col items-center">
+        <div class="text-xl font-bold text-white sm:text-2xl md:text-3xl">
+          1000+
+        </div>
+        <div class="text-sm text-emerald-200 sm:text-base">
+          Clientes satisfeitos
+        </div>
+      </div>
+      <!-- stat - end -->
+
+      <!-- stat - start -->
+      <div class="flex flex-col items-center">
+        <div class="text-xl font-bold text-white sm:text-2xl md:text-3xl">
+          1000+
+        </div>
+        <div class="text-sm text-emerald-200 sm:text-base">
+          Industrias atendidas
+        </div>
+      </div>
+      <!-- stat - end -->
+    </div>
+  </div>
+</div>
+<!-- call to action - end -->
+
+<!-- collections - start -->
+<div class="mx-auto w-full -mt-1">
+  <section
+    class="min-h-96 relative flex flex-1 shrink-0 items-center justify-center overflow-hidden bg-gray-100 py-16 shadow-lg md:py-20 xl:py-48"
+  >
+    <!-- image - start -->
+    <img
+      src="/cano.jpg?auto=format&amp;q=75&amp;fit=crop&amp;w=1500"
+      loading="lazy"
+      alt="Photo by Fakurian Design"
+      class="absolute inset-0 h-full w-full object-cover object-center"
+      style="opacity: 1;"
+    />
+    <!-- image - end -->
+
+    <!-- overlay - start -->
+    <div class="absolute inset-0 bg-black/60 mix-blend-multiply" />
+    <!-- overlay - end -->
+
+    <!-- text start -->
+    <div class="relative flex flex-col items-center p-4 sm:max-w-3xl">
+      <p
+        class="mb-4 text-center justify-center text-lg text-white sm:text-xl md:mb-8"
+      >
+        Busco realizar os trabalhos com máxima rapidez sem perder a qualidade. <br
+        />
+        Para mim segurança vem sempre em primeiro lugar. Desde a abertura da empresa
+        sempre visei trabalhar com dedicação e tentar realizar os trabalhos com excelência.
+        Aminha maior realização é ver que ao final da obra realizei um trabalho com
+        qualidade e que o mesmo trouxe a satisfação total de meus Clientes.
+      </p>
+    </div>
+    <!-- text end -->
+  </section>
+</div>
+<!-- collections - end -->
+
+<!-- newsletter - start -->
+<div
+      class="hero bg-base-100 rounded-lg text-primary-content place-items-center font-semibold"
+    >
+      <div class="flex flex-wrap -mx-3">
+        <div
+          class="mx-auto max-w-screen-xl px-6 pt-16 md:pt-28 s-Ew075HtTebna s-H6zKD9rU5-h6"
+        >
+          <h2
+            class=" text-center text-2xl font-bold text-gray-800  lg:text-3xl"
+          >
+            Entre em contato
+          </h2>
+        </div>
+        <div class="flex w-full mx-auto mt-16 text-center justify-center">
+            <a
+              href="https://www.instagram.com/rwsolucoesindustriais"
+              target="_blank"
+              class="mr-6 text-slate-400"
+              ><span
+                class="text-3xl text-emerald-500 fab fa-instagram"
+                aria-hidden="true"
+              /></a
+            >
+            <a
+              href="https://wa.me/5517996453211"
+              target="_blank"
+              class="mr-6 text-slate-400"
+              ><i class="text-3xl text-emerald-500 fa-brands fa-whatsapp" /></a
+            >
+        </div>
+      </div>
+    </div>
+<!-- newsletter - end -->
+
+<!-- footer - start -->
+<div class="bg-white pt-4 sm:pt-10 lg:pt-12">
+  <footer class="mx-auto max-w-screen-2xl px-4 md:px-8">
+    <div class="flex flex-col items-center border-t pt-6">
+
+      <!-- nav - end -->
+
+      <!-- social - start -->
+      <div class="flex gap-4">
+        <a href="https://www.linkedin.com/in/matheus-gabriel-sa/" target="_blank" class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
+          <svg class="h-5 w-5" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+          </svg>
+        </a>
+
+        <a href="https://wa.me/5517997123536" target="_blank" class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
+          <i class="fa-brands fa-whatsapp fa-xl" />
+        </a>
+
+        <a href="https://github.com/MatheusSaS" target="_blank" class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
+          <svg class="h-5 w-5" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+          </svg>
+        </a>
+        
+      </div>
+      <!-- social - end -->
+    </div>
+
+    <div class="py-8 text-center text-sm text-gray-400">© 2023 - Present Matheus Sá. All rights reserved.</div>
+  </footer>
+</div>
+<!-- footer - end -->
